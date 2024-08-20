83e3416d2525927be91cccda636b896a69164218
MiniEnemy <home.minj@gmail.com>
2024-08-20 08:56:52 +0545

    changes

--- gamergaun/src/App.jsx
+++ gamergaun/src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,8 +12,13 @@ function App() {
 //   font: '2rem sans-serif',  // inline css
 // }
     return ( 
-      <h1 className='text-red-500'> hello mah nigga</h1>
+      // // <h1 className='text-red-500'> hello mah nigga</h1>
+      // when we write <h1> helloworld</h1>
+      // then it converts it to below text
+      // React.createelement("h1",null,"helloworld"),
+      // React.createelement("p",null,"Hellow world 18")
+      <h1>hello</h1>
     )
-}
+};
 
 export default App
