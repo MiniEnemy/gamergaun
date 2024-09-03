56b9f9b09260a585937317a1c4d20ad581b3e5f9
MiniEnemy <home.minj@gmail.com>
2024-09-03 09:43:18 +0545

    did prodcut and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,61 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import TopHeader from './components/topheader/TopHeader';
-import Home from './pages/home/Home';
-import Login from './helpers/login/Login.jsx';
-import Register from '../src/helpers/register/Register.jsx';
-import Contact from './helpers/contact/Contact.jsx';
-import Privacy from './helpers/privacy/Privacy.jsx';
-import Terms from './helpers/t&c/Terms.jsx';
-import PageNotFound from './pages/notfound/pagenotfound.jsx';
-import Aboutus from './pages/aboutus/AboutUs.jsx';
-import LearnMore from '../src/pages/learnmore/learnmore.jsx';
-import { useEffect } from 'react';
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import TopHeader from "./components/topheader/TopHeader";
+import Home from "./pages/home/Home";
+import Login from "./helpers/login/Login.jsx";
+import Register from "../src/helpers/register/Register.jsx";
+import Contact from "./helpers/contact/Contact.jsx";
+import Privacy from "./helpers/privacy/Privacy.jsx";
+import Terms from "./helpers/t&c/Terms.jsx";
+import PageNotFound from "./pages/notfound/pagenotfound.jsx";
+import Aboutus from "./pages/aboutus/AboutUs.jsx";
+import LearnMore from "../src/pages/learnmore/learnmore.jsx";
+import Productview from "./pages/product/productview/Productview.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <>
-  <Router>
-    <TopHeader title="GamerGaun Rewards" />
-    <Header />
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/Register" element={<Register />} />
-      <Route exact path="/Contact" element={<Contact />} />
-      <Route exact path="/privacy" element={<Privacy />} />
-      <Route exact path="/Terms" element={<Terms />} />
-      <Route exact path="*" element={<PageNotFound />} />
-      <Route exact path="/Aboutus" element={<Aboutus />} />
-      <Route exact path="/more-info" element={<LearnMore />
-} />
+  const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
-      
-      
-    </Routes>
-    <Footer />
-  </Router>
-  </>
-  
-  
+  useEffect(() => {
+    const fetchedData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products`);
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchedData();
+  }, []);
+
+  return (
+
+    <Router>
+      <ToastContainer />
+      <TopHeader title="GamerGaun Rewards" />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/Register" element={<Register />} />
+        <Route exact path="/Contact" element={<Contact />} />
+        <Route exact path="/privacy" element={<Privacy />} />
+        <Route exact path="/Terms" element={<Terms />} />
+        <Route exact path="/Aboutus" element={<Aboutus />} />
+        <Route exact path="/more-info" element={<LearnMore />} />
+        <Route exact path="/Productview/:id" element={<Productview isLoading={isLoading} product={isLoading} products={products} />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- src/helpers/login/Login.jsx
+++ src/helpers/login/Login.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from 'react-router-dom';
-
+import { toast } from "react-toastify"; // Use 'toast' instead of 'Toast'
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+  const [loginValue, setLoginValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setLoginValue({ ...loginValue, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(loginValue),
+      };
+
+      const res = await fetch('http://httpbin.org/post', config); // Include 'config' in the fetch request
+
+      if (res.ok) {
+        toast.success("Successfully logged in");
+      } else {
+        toast.error("Failed to login");
+      }
+    } catch (error) {
+      toast.error("An error occurred. Please try again.");
+      console.log("Error:", error);
+    }
+  };
+
   return (
     <>
       <div className="font-poppins max-w-md mx-auto mt-8 p-4 bg-white shadow-md rounded-md my-2">
         <h2 className="text-2xl font-semibold mb-4">Login</h2>
         <span>Please enter your valid credentials</span>
-        <form action="">
+        <form onSubmit={handleSubmit} className="mt-8">
           <div className="mb-4">
             <label
               htmlFor="email"
@@ -22,6 +55,8 @@ const Login = () => {
               name="email"
               placeholder="Enter your email"
               className="mt-1 px-2 py-3 border border-gray-300 rounded-md w-full focus:ring-indigo-500 text-sm"
+              value={loginValue.email}
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -37,15 +72,23 @@ const Login = () => {
               name="password"
               placeholder="Enter your password"
               className="mt-1 px-2 py-3 border border-gray-300 rounded-md w-full focus:ring-indigo-500 text-sm"
+              value={loginValue.password}
+              onChange={handleChange}
             />
           </div>
           <div className="mt-4">
-            <button className="py-2 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500">
+            <button
+              type="submit"
+              className="py-2 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500"
+            >
               Login
             </button>
           </div>
-          <span className="my-2 flex justify-center items-center">Don't have an account? 
-            <NavLink to="/register" className="text-indigo-600 hover:text-indigo-800 text-decoration-none ">Register</NavLink>
+          <span className="my-2 flex justify-center items-center">
+            Don't have an account?{" "}
+            <NavLink to="/register" className="text-indigo-600 hover:text-indigo-800 text-decoration-none ml-1">
+              Register
+            </NavLink>
           </span>
         </form>
       </div>
